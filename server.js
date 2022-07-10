3aed20c452560a83225195eab029826f04bdeb1c
chrisdhodges12 <102702357+chrisdhodges12@users.noreply.github.com>
2022-07-10 15:20:37 -0400

    functions to view all employees and add a new role

--- server.js
+++ server.js
@@ -38,19 +38,19 @@ function initialPrompt() {
       if (main === "View all roles") {
         viewRoles();
       }
-      if ((main = "View all employees")) {
+      if ((main === "View all employees")) {
         viewEmployees();
       }
-      if ((main = "Add a department")) {
+      if ((main === "Add a department")) {
         addDepartment();
       }
-      if ((main = "Add a role")) {
+      if ((main === "Add a role")) {
         addRole();
       }
-      if ((main = "Add an employee")) {
+      if ((main === "Add an employee")) {
         addEmployee();
       }
-      if ((main = "Update an employee role")) {
+      if ((main === "Update an employee role")) {
         updateEmployeeRole();
       }
     });
@@ -82,9 +82,49 @@ viewRoles = () => {
   });
 };
 
-viewEmployees = () => {};
+viewEmployees = () => {
+  const sql = `SELECT employee.id,
+                employee.first_name,
+                employee.last_name,
+                department.department_name AS department,
+                role.title,
+                role.salary,
+                CONCAT (manager.first_name, " ", manager.last_name) AS manager
+                FROM employee
+                LEFT JOIN role ON employee.role_id = role.id
+                LEFT JOIN department ON role.department_id = department.id
+                LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      return;
+    }
+    console.log("");
+    console.table(rows);
+    initialPrompt();
+  });
+};
 
-addDepartment = () => {};
+addDepartment = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'newDep',
+        message: 'What is the name of the department you would like to add?',
+      },
+    ])
+    .then(answer => {
+      const sql = `INSERT INTO department (department_name) VALUES (?)`;
+      db.query(sql, answer.newDep, (err, result) => {
+        if (err) {
+          return;
+        }
+        console.log(""),
+        console.log("A new department was added");
+        viewDepartments();
+      });
+    });
+};
 
 addRole = () => {};
 
